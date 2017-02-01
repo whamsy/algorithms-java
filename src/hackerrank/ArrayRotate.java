c200e142c400c77da0798f1a2c3d6f4e51f8e03d
whamsy <waatluri@gmail.com>
2017-02-01 12:42:59 -0500

    Started Rotate and Array problem

---
+++ src/hackerrank/ArrayRotate.java
@@ -0,0 +1,16 @@
+package hackerrank;
+
+public class ArrayRotate{
+	
+	public static void main(String args[]){
+		
+		int n = 5; //number of elements
+		int d = 4; //number of rotations to be done
+		int a[] = {1,2,3,4,5}; //array to be rotated
+		
+		
+		
+		
+		
+	}
+}

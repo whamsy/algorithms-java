d8ea807f90fc8d70e19fa6eaee0513672b7e63eb
whamsy <waatluri@gmail.com>
2017-02-02 18:11:00 -0500

    Adding code

--- src/coderust/ArrayBinarySearch.java
+++ src/coderust/ArrayBinarySearch.java
@@ -8,28 +8,21 @@ public class ArrayBinarySearch{
 		
 		int array[] = {1,10,20,47,59,63,75,88,99,107,120,133,155,162,176,188,199,200,210,222};
 		
-<<<<<<< HEAD
+
 		int find = 88;
-=======
-		int find = 47;
->>>>>>> master
+
 		int low = 0;
 		int high = array.length;
 		
-		
-<<<<<<< HEAD
 		System.out.println(binarySearch(array,find,low,high));
-=======
-		System.out.println(binarySearch(array[],find));
->>>>>>> master
-		
+
 		
 	}
 	
 	public static int binarySearch(int input[],int x, int low, int high){
 		
 		int mid = (low+high)/2;
-<<<<<<< HEAD
+
 		
 		if (low > high) return -1;
 		
@@ -40,12 +33,5 @@ public class ArrayBinarySearch{
 		} else {
 			return binarySearch(input, x, mid + 1, high);
 		}
-		
-		
-=======
-		if(x < input[mid]){
-			high = mid
-		}
->>>>>>> master
 	}
 }

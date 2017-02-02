3938c3d22208fa2e49fcadba1b5fb344698bed40
whamsy <waatluri@gmail.com>
2017-02-02 18:16:11 -0500

    Adding code

---
+++ src/coderust/MaxInSlidingWindow.java
@@ -0,0 +1,13 @@
+package coderust;
+
+//Given a large array of integers and a window of size 'w', 
+//find the current maximum in the window as the window slides through the entire array.
+
+public class MaxInSlidingWindow{
+	
+	public static void main(String args[]){
+		
+		
+		
+	}
+}

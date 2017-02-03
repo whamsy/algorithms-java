2af8532d6e58bae486a96b3fbf977f1c7fa84c0d
whamsy <waatluri@gmail.com>
2017-02-02 19:26:58 -0500

    Completed MaxInSlidingWindow problem

--- src/coderust/MaxInSlidingWindow.java
+++ src/coderust/MaxInSlidingWindow.java
@@ -1,11 +1,50 @@
 package coderust;
 
+import java.util.ArrayDeque;
+
 //Given a large array of integers and a window of size 'w', 
 //find the current maximum in the window as the window slides through the entire array.
 
 public class MaxInSlidingWindow{
 	
 	public static void main(String args[]){
+		
+		int array[] = {-4,2,-5,3,6};
+		int w = 3;
+		
+		ArrayDeque<Integer> window = new ArrayDeque<>();
+		
+		for(int i = 0; i < w; i++){
+			
+			while(!window.isEmpty() && array[i]>window.peekLast()){
+				
+				window.removeLast();
+				
+			}
+			
+			window.addLast(i);
+		}
+		
+		System.out.println(array[window.peekFirst()]+",");
+		
+		for(int i=w; i < array.length; i++){
+			
+				while(!window.isEmpty() && array[i]>window.peekLast()){
+					
+					window.removeLast();
+				
+				}
+				
+				while(!window.isEmpty() && window.peekFirst()<i-w){
+					
+					window.removeFirst();
+					
+				}
+				
+				window.addLast(i);
+				
+				System.out.println(array[window.peekFirst()]+",");
+		}
 		
 		
 		

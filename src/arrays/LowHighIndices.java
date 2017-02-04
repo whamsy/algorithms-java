b36fbff696fcd47fef3e2ffbef85a66138e0a6c9
whamsy <waatluri@gmail.com>
2017-02-04 17:22:51 -0500

    Adding code

---
+++ src/arrays/LowHighIndices.java
@@ -0,0 +1,10 @@
+package arrays;
+
+//Given a sorted array of integers, return the low and high index of the given key. 
+//Return -1 if not found. The array length can be in millions with lots of duplicates
+
+public class LowHighIndices{
+	public static void main(String args[]){
+		
+	}
+}

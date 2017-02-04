5e94da842f31fcb289d01c9cc97de74e13115a74
whamsy <waatluri@gmail.com>
2017-02-04 17:26:35 -0500

    Adding code LowHighIndex

--- src/arrays/LowHighIndices.java
+++ src/arrays/LowHighIndices.java
@@ -6,5 +6,21 @@ package arrays;
 public class LowHighIndices{
 	public static void main(String args[]){
 		
+		int a[] = {1,2,2,2,5,5,5,5,5,5,10,10,20};
+		int k = 5;
+		
+		System.out.println(low_index(a,k));
+		System.out.println(high_index(a,k));
+		
+	}
+
+	private static char[] high_index(int[] a, int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static char[] low_index(int[] a, int k) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }

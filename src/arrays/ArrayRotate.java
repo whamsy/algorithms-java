ff7273228906d2c091d9edb1377e9e86c4a3edbc
whamsy <waatluri@gmail.com>
2017-02-04 17:05:21 -0500

    Added Array Rotate

---
+++ src/arrays/ArrayRotate.java
@@ -0,0 +1,46 @@
+package arrays;
+
+import java.util.Arrays;
+
+//Given an array of integers, rotate the array by 'N' elements.
+
+public class ArrayRotate{
+	public static void main(String args[]){
+		int a[] = {1,2,3,4,5,6,7}; //array to be rotated
+		int d = 1; //number of rotations, '+' -> right, '-' -> left
+		
+		System.out.println(Arrays.toString(a));
+		
+		//System.out.print(Arrays.toString(rotate(a,d)));
+		reverse_array(a, 0, a.length-1);
+		System.out.println(Arrays.toString(a));
+		
+		reverse_array(a, 0, 1);
+		System.out.println(Arrays.toString(a));
+		
+		
+	}
+
+	private static int[] rotate(int[] a, int d) {
+		
+		int len = a.length;
+		
+		reverse_array(a,0,len-1);
+		reverse_array(a, 0, d);
+		reverse_array(a, d, len-1);
+		
+		return a;
+		
+	}
+
+	private static void reverse_array(int[] a, int i, int j) {
+		
+		while(i<j){
+			int temp = a[j];
+			a[j] = a[i];
+			a[i] = temp;
+			i++;
+			j--;
+		}
+	}
+}
--- src/hackerrank/ArrayLeftRotate.java
+++ src/hackerrank/ArrayLeftRotate.java
@@ -9,7 +9,7 @@ public class ArrayLeftRotate{
 	public static void main(String args[]){
 		
 		int n = 5; //number of elements
-		int d = 1; //number of rotations to be done
+		int d = 2; //number of rotations to be done
 		int a[] = {1,2,3,4,5}; //array to be rotated
 		int out[] = new int[n];
 		

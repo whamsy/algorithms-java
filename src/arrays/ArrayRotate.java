9cad3cd6e0312e45719dcdd8173c259d25be993b
whamsy <waatluri@gmail.com>
2017-02-04 17:17:03 -0500

    Code Edited

--- src/arrays/ArrayRotate.java
+++ src/arrays/ArrayRotate.java
@@ -7,26 +7,18 @@ import java.util.Arrays;
 public class ArrayRotate{
 	public static void main(String args[]){
 		int a[] = {1,2,3,4,5,6,7}; //array to be rotated
-		int d = 4; //number of rotations, '+' -> right, '-' -> left
-		
-		System.out.println(Arrays.toString(a));
+		int d = -4; //number of rotations, '+' -> right, '-' -> left
 		
 		System.out.print(Arrays.toString(rotate(a,d)));
-//		reverse_array(a, 0, a.length-1);
-//		System.out.println(Arrays.toString(a));
-//		
-//		reverse_array(a, 0, 1-1);
-//		System.out.println(Arrays.toString(a));
-//		
-//		reverse_array(a, 1, a.length-1);
-//		System.out.println(Arrays.toString(a));
-		
-		
 	}
 
 	private static int[] rotate(int[] a, int d) {
-		
+
 		int len = a.length;
+		
+		if(d<0){
+			d = a.length + d;
+		}
 		
 		reverse_array(a,0,len-1);
 		reverse_array(a, 0, d-1);

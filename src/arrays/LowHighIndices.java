e020a89bf99da7256e6912104a17c6f266c488c5
whamsy <waatluri@gmail.com>
2017-02-04 17:46:11 -0500

    Completed code
    Low High Indices

--- src/arrays/LowHighIndices.java
+++ src/arrays/LowHighIndices.java
@@ -7,9 +7,9 @@ public class LowHighIndices{
 	public static void main(String args[]){
 		
 		int a[] = {1,2,2,2,5,5,5,5,5,5,10,10,20};
-		int k = 2;
+		int k = 5;
 		
-		//System.out.println(low_index(a,k));
+		System.out.println(low_index(a,k));
 		System.out.println(high_index(a,k));
 		
 	}
@@ -18,10 +18,10 @@ public class LowHighIndices{
 		
 		int start = 0;
 		int end = a.length - 1;
-		//int mid = (start+end)/2;
+		int mid = (start+end)/2;
 		
-		while(start<end){
-			int mid = (start+end)/2;
+		while(start<=end){
+			mid = (start+end)/2;
 			if(a[mid]<=k){
 				start = mid + 1;
 			}
@@ -29,14 +29,33 @@ public class LowHighIndices{
 				end = mid - 1;
 			}
 		}
+		
 		if (a[end] == k) 
 			return end;
 		else 
 			return -1;
 	}
 
-	private static char[] low_index(int[] a, int k) {
-		// TODO Auto-generated method stub
-		return null;
+	private static int low_index(int[] a, int k) {
+		
+		int start = 0;
+		int end = a.length - 1;
+		int mid = (start+end)/2;
+		
+		while(start<=end){
+			mid = (start+end)/2;
+			if(a[mid]>=k){
+				end = mid - 1;
+			}
+			else {
+				start = mid + 1;
+			}
+		}
+		
+		if (a[start] == k) 
+			return start;
+		else 
+			return -1;
+		
 	}
 }

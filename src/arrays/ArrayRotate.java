a1d63c7bda13b6f5f66f174c025b7bb139f0415d
whamsy <waatluri@gmail.com>
2017-02-04 17:07:59 -0500

    Code Edits

--- src/arrays/ArrayRotate.java
+++ src/arrays/ArrayRotate.java
@@ -7,16 +7,19 @@ import java.util.Arrays;
 public class ArrayRotate{
 	public static void main(String args[]){
 		int a[] = {1,2,3,4,5,6,7}; //array to be rotated
-		int d = 1; //number of rotations, '+' -> right, '-' -> left
+		int d = 4; //number of rotations, '+' -> right, '-' -> left
 		
 		System.out.println(Arrays.toString(a));
 		
-		//System.out.print(Arrays.toString(rotate(a,d)));
-		reverse_array(a, 0, a.length-1);
-		System.out.println(Arrays.toString(a));
-		
-		reverse_array(a, 0, 1);
-		System.out.println(Arrays.toString(a));
+		System.out.print(Arrays.toString(rotate(a,d)));
+//		reverse_array(a, 0, a.length-1);
+//		System.out.println(Arrays.toString(a));
+//		
+//		reverse_array(a, 0, 1-1);
+//		System.out.println(Arrays.toString(a));
+//		
+//		reverse_array(a, 1, a.length-1);
+//		System.out.println(Arrays.toString(a));
 		
 		
 	}
@@ -26,7 +29,7 @@ public class ArrayRotate{
 		int len = a.length;
 		
 		reverse_array(a,0,len-1);
-		reverse_array(a, 0, d);
+		reverse_array(a, 0, d-1);
 		reverse_array(a, d, len-1);
 		
 		return a;

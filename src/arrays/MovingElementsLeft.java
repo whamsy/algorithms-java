6be81ae37d8a36d97302fb55ec25218c1f270fd0
whamsy <waatluri@gmail.com>
2017-02-06 13:27:07 -0500

    Adding new program

---
+++ src/arrays/MovingElementsLeft.java
@@ -0,0 +1,8 @@
+package arrays;
+//Given an integer array, move all elements containing '0' to the left 
+//while maintaining the order of other elements in the array.
+public class MovingElementsLeft{
+	public static void main(String args[]){
+		
+	}
+}

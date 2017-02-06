a075bf558ef5b3a47b221353a0ded7da80271e25
whamsy <waatluri@gmail.com>
2017-02-06 13:41:18 -0500

    Adding new program

---
+++ src/arrays/MaxSingleSellProfit.java
@@ -0,0 +1,7 @@
+package arrays;
+//Given a list of stock prices for n days, find the maximum profit with a single buy/sell activity.
+public class MaxSingleSellProfit{
+	public static void main(String args[]){
+		
+	}
+}

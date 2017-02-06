3ee09c96d3ad0e62a3fe08e5ae6ebdb998d0c2aa
whamsy <waatluri@gmail.com>
2017-02-06 16:34:51 -0500

    Testing logic

--- src/arrays/QuickSort.java
+++ src/arrays/QuickSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 //Given an integer array, sort it in ascending order using quicksort.
 public class QuickSort{
 	public static void main(String args[]){
-		int a[] = {55,23,26,2,25};
+		int a[] = {23,26,2,25};
 		
 		quick_sort(a,0,a.length-1);
 		
@@ -15,9 +15,10 @@ public class QuickSort{
 	private static void quick_sort(int[] a,int low, int high) {
 		
 		if (high>low){
-			System.out.println(low+" "+high);
+			System.out.println("Low:"+low+" high:"+high);
 			int pivot = partition_array(a,low,high);
-			
+			System.out.println(Arrays.toString(a));
+			System.out.println("pivot:"+pivot);
 			quick_sort(a,low,pivot-1);
 			quick_sort(a,pivot+1,high);
 		}
@@ -26,17 +27,17 @@ public class QuickSort{
 	private static int partition_array(int[] arr, int low, int high) {
 		
 		int pivot_value = arr[low];
-		System.out.println(pivot_value);
-		  int i = low;
-		  int j = high;
+		int i = low;
+		int j = high;
 
 		  while (i < j) {
 		    while (arr[i] <= pivot_value && i < high) 
 		    	{
-		    	 System.out.println(arr[i]);
 		    	 i++;
 		    	}
+		    System.out.println("i="+i);
 		    while (arr[j] > pivot_value) j--;
+		    System.out.println("j="+j);
 
 		    if (i < j) {
 		      // swap arr[i] and arr[j]

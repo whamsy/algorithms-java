2e342230aa2bda89acbbe5fef6dc68decc85902f
whamsy <waatluri@gmail.com>
2017-02-07 22:16:47 -0500

    Adding code

---
+++ src/linkedlists/ReverseSinglyLinked.java
@@ -0,0 +1,12 @@
+package linkedlists;
+/**
+ * Given the pointer/reference to the head of a singly linked list, 
+ * reverse it and return the pointer/reference to the head of reversed linked list.
+*/
+
+public class ReverseSinglyLinked{
+	public static void main(String args[]){
+		
+		
+	}
+}

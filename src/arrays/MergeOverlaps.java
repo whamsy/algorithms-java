54630ee260c59ffd47d8c9efe4e5de86dcb16758
whamsy <waatluri@gmail.com>
2017-02-08 02:38:36 -0500

    Changes to Merge Overlaps

--- src/arrays/MergeOverlaps.java
+++ src/arrays/MergeOverlaps.java
@@ -1,38 +1,58 @@
 package arrays;
 
+import java.awt.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //Given an array (list) of intervals as input where each interval has a start and end timestamps.
 //Input array is sorted by starting timestamps. You are required to merge overlapping intervals 
 //and return output array (list).
-//Consider below input array. Intervals (1, 5), (3, 7), (4, 6), (6, 8) are overlapping 
+//Consider input array[(1, 5), (3, 7), (4, 6), (6, 8),(10, 12) and (11, 15)]. Intervals (1, 5), (3, 7), (4, 6), (6, 8) are overlapping 
 //so should be merged to one big interval (1, 8). Similarly intervals (10, 12) and (11, 15) 
 //are also overlapping intervals and should be merged to (10, 15).
 public class MergeOverlaps{
+	
+	static class Tuple { 
+		public Integer x;
+		public Integer y;
+		
+		public Tuple(int a, int b) {
+			this.x = a; 
+		    this.y = b;
+		    }
+		
+		@Override
+        public String toString() {
+            return x.toString()+"-"+y.toString();
+        }
+	}
+	
 	public static void main(String args[]){
-		ArrayList<Tuple<Integer,Integer>> test = new ArrayList<Tuple<Integer,Integer>>();
+		
+		ArrayList<Tuple> test = new ArrayList<Tuple>();
+		Tuple tuple1 = new Tuple(1,5);
+		Tuple tuple2 = new Tuple(3,7);
+		Tuple tuple3 = new Tuple(4,6);
+		Tuple tuple4 = new Tuple(6,8);
+		Tuple tuple5 = new Tuple(10,12);
+		Tuple tuple6 = new Tuple(11,15);
+		test.add(tuple1);
+		test.add(tuple2);
+		test.add(tuple3);
+		test.add(tuple4);
+		test.add(tuple5);
+		test.add(tuple6);
+		
+		System.out.println(test);
+		
+		merge_overlaps(test);
 		
 		
 	}
-	
-	public class Pair<F,S>{
+
+	private static void merge_overlaps(ArrayList<Tuple> test) {
 		
-		public F first;
-		public S second;
 		
-		public Pair(F first, S second){
-			this.first = first;
-			this.second = second;
-		}
 		
 	}
-	
-	public class Tuple<X, Y> { 
-		  public final X x; 
-		  public final Y y; 
-		  public Tuple(X x, Y y) { 
-		    this.x = x; 
-		    this.y = y; 
-		  } 
-		} 
 }

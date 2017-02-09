787546a1ab4fb269b46edf0f2793acde6bb15179
whamsy <waatluri@gmail.com>
2017-02-09 17:44:29 -0500

    Code Edits

--- src/linkedlists/SinglyLinkedList.java
+++ src/linkedlists/SinglyLinkedList.java
@@ -1,7 +1,5 @@
 package linkedlists;
 
-import java.util.LinkedList;
-
 /**
  * Given the pointer/reference to the head of a singly linked list, 
  * reverse it and return the pointer/reference to the head of reversed linked list.
@@ -16,29 +14,57 @@ public class SinglyLinkedList{
 		public int data;
 		public Node next;
 		
-		Node(int a, Node next){
+		Node(int a){
 			this.data = a;
-			this.next = next;
+			this.next = null;
 		}
 	}
 	
+	static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+	
 	public static void main(String args[]){
 		
-		LinkedList<Integer> list1 = new LinkedList<>();
-		list1.add(7);
-		list1.add(14);
-		list1.add(21);
-		list1.add(28);
+		SinglyLinkedList list1 = new SinglyLinkedList();
+		list1.head = new Node(7);
+		list1.head.next = new Node(14);
+		list1.head.next.next = new Node(21);
+		list1.head.next.next.next = new Node(28);
 		
-		System.out.println(list1);
-		reverse_list(list1);
+		printList(head);
 		
+		reverse_list(head);
+		
+		printList(reverse_list(head));
 	}
-	
-	
 
-	private static void reverse_list(LinkedList<Integer> list1) {
-		// TODO Auto-generated method stub
+	private static Node reverse_list(Node head2) {
+		
+		if(head2==null||head2.next==null){
+			return null;
+		}
+		
+		Node start = head2;
+		Node point = head2.next;
+		
+		start.next = null;
+		
+		while(point != null){
+			Node temp = point.next;
+			System.out.println("test2");
+			point.next = start;
+			
+			point = temp;
+			
+		}
+		
+		head2 = start;
+		return head2;
+		
 		
 	}
 }

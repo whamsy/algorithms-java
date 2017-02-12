0ad09af56ffcdab32d5a11a258268a6d6a33d165
whamsy <waatluri@gmail.com>
2017-02-11 23:57:28 -0500

    Completed Queue using Stack prog

--- src/stacks_queues/QueueUsingStacks.java
+++ src/stacks_queues/QueueUsingStacks.java
@@ -1,9 +1,53 @@
 package stacks_queues;
 
-public class QueueUsingStacks {
+import java.util.Stack;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class QueueUsingStacks {
+	
+	Stack<Integer> stack1 = new Stack<Integer>(); 
+	Stack<Integer> stack2 = new Stack<Integer>(); 
+
+		  void enqueue(int data) {
+		    stack1.push(data);
+		  }
+
+		  boolean isEmpty() {
+		    return stack1.size() + stack2.size() == 0;
+		  }
+
+		  int dequeue() throws Exception {
+		    if(isEmpty()) {
+		      throw new Exception("queue is empty");
+		    }
+
+		    if(stack2.isEmpty()) {
+		      while(!stack1.isEmpty()){
+		          stack2.push(stack1.pop());
+		      }
+		    }
+
+		    return stack2.pop();
+		  }
+		
+
+	public static void main(String[] args) throws Exception {
+		
+		QueueUsingStacks q1 = new QueueUsingStacks();
+		
+		q1.enqueue(1);
+		q1.enqueue(2);
+		q1.enqueue(3);
+		
+		System.out.println(q1.dequeue());
+		System.out.println(q1.dequeue());
+		
+		q1.enqueue(4);
+		q1.enqueue(5);
+		q1.enqueue(6);
+		
+		System.out.println(q1.dequeue());
+		System.out.println(q1.dequeue());
+
 
 	}
 
